67e00a2898a8e967e1c7e0115eb332233c6628c7
Chardonnay <konomusicbox6@gmail.com>
2018-05-17 02:25:57 +0900

    lesson_finish

--- js/main.js
+++ js/main.js
@@ -4,19 +4,29 @@
 	var btn = document.getElementById('btn');
 
 	btn.addEventListener('click',function(){
-		var n = Math.floor(Math.random() * 3);
-		switch (n){
-			case 0:
-				this.textContent = '大吉';
-				break;
-			case 1:
-				this.textContent = '中吉';
-				break;
-			case 2:
-				this.textContent = '凶';
-				break;
-
+		var n = Math.random();
+		if (n < 0.05){
+			this.textContent = '大吉'; // 5%
+		}else if (n < 0.2){
+			this.textContent = '中吉'; // 5%
+		}else{
+			this.textContent = '凶'; // 5%
 		}
+		// var results = ['大吉', '中吉', '凶', '末吉']
+		// var n = Math.floor(Math.random() * results.length);
+		// this.textContent = results[n];
+		// switch (n){
+		// 	case 0:
+		// 		this.textContent = '大吉';
+		// 		break;
+		// 	case 1:
+		// 		this.textContent = '中吉';
+		// 		break;
+		// 	case 2:
+		// 		this.textContent = '凶';
+		// 		break;
+
+		// }
 	// 	if (n == 0){
 	// 		this.textContent = '大吉';
 	// 	}else if (n == 1){
